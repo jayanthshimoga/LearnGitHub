716d4d785045043043f09699311e24b18c7cdf02
jayanth <jayanthzlak@gmail.com>
2023-09-22 20:24:22 -0400

    Initial Commit

--- test/learn_git_hub.test.ts
+++ test/learn_git_hub.test.ts
@@ -1,17 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as LearnGitHub from '../lib/learn_git_hub-stack';
+// import * as cdk from 'aws-cdk-lib';
+// import { Template, Match } from 'aws-cdk-lib/assertions';
+// import * as LearnGitHub from '../lib/learn_git_hub-stack';
 
-test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new LearnGitHub.LearnGitHubStack(app, 'MyTestStack');
-  // THEN
+// test('SQS Queue and SNS Topic Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new LearnGitHub.LearnGitHubStack(app, 'MyTestStack');
+//   // THEN
 
-  const template = Template.fromStack(stack);
+//   const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
-});
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+//   template.resourceCountIs('AWS::SNS::Topic', 1);
+// });
